18653c3d6fa5e2c276e2414697094bdd0adf9dbb
Neeraj Singh <singh4neeraj@gmail.com>
2021-01-06 02:16:24 +0530

    'ecomfront'

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -10,7 +10,7 @@ const Home = props => {
 
 	useEffect(() => {
 		onLoad();
-	}, []);
+	}, [onLoad]);
 
 	let history = useHistory();
 	
--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
 import styles from './style.module.css';
 
 import ProductCard from './../productCard'
